540726c6997e6842ff3fd0ad432658c778e1a6d8
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-12-09 17:36:03 +0530

    Add files via upload

---
+++ Circumference/main.py
@@ -0,0 +1,6 @@
+def circumference():
+    diameter = int(input("Enter the diameter of the circle: "))
+    circumference1 = 3.14*diameter
+    print("The circumference is", circumference1)
+
+circumference()
